0939f6c24b6940949b2f33f0b977b64bbe0c366c
Homer <Home@fdasdar>
2023-05-09 22:22:09 +0300

    adj for vercel

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 // server.js
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('heroes.json')
+// const router = jsonServer.router('heroes.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use(router)
+// server.use(router)
 server.listen(3001, () => {
   console.log('JSON Server is running')
 })
